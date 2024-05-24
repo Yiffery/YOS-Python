2f1792c305413a4859576d75ea4f56e1c7e810f4
Yifei Wang <128399837+Yiffery@users.noreply.github.com>
2024-05-24 13:28:52 +0000

    Notepad app updates

--- home.py
+++ home.py
@@ -7,32 +7,55 @@ def clear():
   system('clear')
 
 
+
+# App Variables
+# Notpad
+note = ""
+
 # Get terminal size
 dimensions = re.findall(r'\b\d+\b', str(shutil.get_terminal_size()))
 width = dimensions[0]
-# Print Title and Subtitle
-print(f'{"YOS Python 0.1":^{width}}')
-print("")
 
-# Apps
-print(f'{"Installed Apps":^{width}}')
-print(f'{"-"*int(width):^{width}}')
-
-print(f'               1. Calculator{" "*(int(width)-54)}2. Notepad               ')
-
-
-# Input
-select = input("Select an app by inputting the corresponding number: ")
-
-if select == "1":
-  print()
-elif select == "2":
+def home_page():
+  # CLEAR IT ALL!
   clear()
-  print("Notepad")
-  print("="*int(width))
-  while True:
-    currentline = input()
-    f = open("notepadsave.txt", "a")
-    f.write(currentline)
-    f.write("\n")
-    f.close()
+  # Get terminal size
+  dimensions = re.findall(r'\b\d+\b', str(shutil.get_terminal_size()))
+  width = dimensions[0]
+  # Print Title and Subtitle
+  print(f'{"YOS Python 0.1":^{width}}')
+  print("")
+  
+  # Apps
+  print(f'{"Installed Apps":^{width}}')
+  print(f'{"-"*int(width):^{width}}')
+  
+  print(f'               1. Calculator{" "*(int(width)-54)}2. Notepad               ')
+  
+  
+  # Input
+  select = input("Select an app by inputting the corresponding number: ")
+
+  if select == "1":
+    print()
+  elif select == "2":
+    clear()
+    note=""
+    print("Notepad (Not saving) (Type /exit to exit)")
+    print("="*int(width))
+    if note != "":
+      print(note)
+    else:
+      
+      while True:
+        currentline = input()
+        if currentline == "/exit":
+          home_page()
+        else:
+          if note != "":
+            note = note + '''
+''' + currentline
+            print(note)
+          else:
+            note = currentline
+home_page()
