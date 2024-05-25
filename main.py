f970e2cbe75f34dad93f517ba26264c50db0e208
Yifei Wang <128399837+Yiffery@users.noreply.github.com>
2024-05-25 23:54:01 +0000

    YOS Python 0.2beta-3
    ALERT: home.py has merged into main.py in this release.
    - New loading screen
    - Is now only one file
    - Renamed some stuff
    - Others stuff + bug fixes
    YOS Python 0.2 should be coming out this weekend or so

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # Boot Manager for YOS Python
 
-# Imports
+# OS SYSTEM IMPORT
+
+
+
 print(" \       /      -----           -----     ")
 print("  \     /     /       \        /          ")
 print("   \   /     /         \      |           ")
@@ -11,12 +14,12 @@ print("     |       \         /              |   ")
 print("     |        \       /              /    ")
 print("     |          -----           -----     ")
 print("                                          ")
-
-# Import Segment 1
 print("Importing: os/System")
-print("[                                        ] 0%")
+print("[=======                                 ] 17%")
 from os import system
 system('clear')
+
+# YOS CLEAR IMPORT
 print(" \       /      -----           -----     ")
 print("  \     /     /       \        /          ")
 print("   \   /     /         \      |           ")
@@ -26,11 +29,56 @@ print("     |      |           |            \    ")
 print("     |       \         /              |   ")
 print("     |        \       /              /    ")
 print("     |          -----           -----     ")
-# Import Functions
-print("Importing: functions")
-print("[====================                    ] 50%")
+print("Importing: YOS/Clear")
+print("[==============                          ] 35%")
 def clear():
   system('clear')
+
+clear()
+
+# REPLIT DB IMPORT
+print(" \       /      -----           -----     ")
+print("  \     /     /       \        /          ")
+print("   \   /     /         \      |           ")
+print("    \ /     |           |      \          ")
+print("     |      |     -     |       -----     ")
+print("     |      |           |            \    ")
+print("     |       \         /              |   ")
+print("     |        \       /              /    ")
+print("     |          -----           -----     ")
+print("Importing: Replit/db")
+print("[====================                    ] 50%")
+from replit import db
+
+clear()
+
+# PYDICTIONARY IMPORT
+print(" \       /      -----           -----     ")
+print("  \     /     /       \        /          ")
+print("   \   /     /         \      |           ")
+print("    \ /     |           |      \          ")
+print("     |      |     -     |       -----     ")
+print("     |      |           |            \    ")
+print("     |       \         /              |   ")
+print("     |        \       /              /    ")
+print("     |          -----           -----     ")
+print("Importing: PyDictionary")
+print("[===========================             ] 67%")
+from PyDictionary import PyDictionary
+clear()
+# SHUTIL IMPORT
+print(" \       /      -----           -----     ")
+print("  \     /     /       \        /          ")
+print("   \   /     /         \      |           ")
+print("    \ /     |           |      \          ")
+print("     |      |     -     |       -----     ")
+print("     |      |           |            \    ")
+print("     |       \         /              |   ")
+print("     |        \       /              /    ")
+print("     |          -----           -----     ")
+print("Importing: Shutil")
+print("[=================================       ] 83%")
+import shutil
 clear()
 print(" \       /      -----           -----     ")
 print("  \     /     /       \        /          ")
@@ -41,22 +89,87 @@ print("     |      |           |            \    ")
 print("     |       \         /              |   ")
 print("     |        \       /              /    ")
 print("     |          -----           -----     ")
-# Import Time
-print("Importing: time")
-print("[===============================         ] 75%")
-import time
-time.sleep(1)
-clear()
-print(" \       /      -----           -----     ")
-print("  \     /     /       \        /          ")
-print("   \   /     /         \      |           ")
-print("    \ /     |           |      \          ")
-print("     |      |     -     |       -----     ")
-print("     |      |           |            \    ")
-print("     |       \         /              |   ")
-print("     |        \       /              /    ")
-print("     |          -----           -----     ")
+print("Importing: re")
 print("[========================================] 100%")
-time.sleep(0.25)
+import re
 clear()
-exec(open("home.py").read())
+  # Imports
+
+
+
+
+
+
+
+
+
+# App Variables
+# Notpad
+
+note = "" 
+# Get terminal size
+dimensions = re.findall(r'\b\d+\b', str(shutil.get_terminal_size()))
+width = dimensions[0]
+def home_page():
+  # CLEAR IT ALL!
+  clear()
+  # Get terminal size
+  dimensions = re.findall(r'\b\d+\b', str(shutil.get_terminal_size()))
+  width = dimensions[0]
+  # Print Title and Subtitle
+  print(f'{"YOS Python 0.2beta-3":^{width}}')
+  print("")
+
+  # Apps
+  print(f'{"Installed Apps":^{width}}')
+  print(f'{"-"*int(width):^{width}}')
+
+  print(f'            1. Information{" "*(int(width)-48)}2. Notepad            ')
+
+
+  # Input
+  select = input("Select an app by inputting the corresponding number: ")
+
+  if select == "1":
+    dict = PyDictionary()
+    def dictionary():
+      exec(open("README.md").read())
+  elif select == "2":
+
+    clear()
+    print("Notepad (Type /exit to exit)")
+    print("="*int(width))
+    # Check if note is blank
+
+    if db.get("note", "empty") != "empty":
+      print(db["note"])
+      while True:
+        currentline = input()
+        # Check if note is blank
+        if currentline == "/exit":
+          home_page()
+        else:
+          if db.get("note", "empty") != "empty":
+            # If note isn't blank, create new line and write
+            db["note"] = db["note"] + '''
+        ''' + currentline
+          else:
+            # If note is blank, don't create new line, but write
+            db["note"] = currentline
+    else:
+      while True:
+        currentline = input()
+        # Check if note is blank
+        if currentline == "/exit":
+          home_page()
+
+        else:
+          if db.get("note", "empty") != "empty":
+            # If note isn't blank, create new line and write
+            db["note"] = db["note"] + '''
+        ''' + currentline
+          else:
+            # If note is blank, don't create new line, but write
+            db["note"] = currentline
+
+home_page()
