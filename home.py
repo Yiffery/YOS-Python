5c89f80fae40cdf1e617c1411dd094ecb0b98792
Yifei Wang <128399837+Yiffery@users.noreply.github.com>
2024-05-25 14:10:06 +0000

    YOS Python 0.2beta-2
    Updated bootloader
    Removed legacy notepad saving
    New notepad saving v2
    Enclaved app support coming soon hopefully

--- home.py
+++ home.py
@@ -1,4 +1,5 @@
 # Imports
+from replit import db
 from os import system
 import shutil
 import re
@@ -10,12 +11,11 @@ def clear():
 
 # App Variables
 # Notpad
-note = ""
 
+note = "" 
 # Get terminal size
 dimensions = re.findall(r'\b\d+\b', str(shutil.get_terminal_size()))
 width = dimensions[0]
-
 def home_page():
   # CLEAR IT ALL!
   clear()
@@ -40,18 +40,39 @@ def home_page():
     print()
   elif select == "2":
     clear()
-    newnote = note
-    print("Notepad (Not saving) (Type /exit to exit)")
+    print("Notepad (Type /exit to exit, /clear to clear)")
     print("="*int(width))
-    while True:
-      currentline = input()
-      if currentline == "/exit":
-        home_page()
-      else:
-        if newnote != "":
-          newnote = newnote + '''
-''' + currentline
-          print(newnote)
+    # Check if note is blank
+  
+    if db.get("note", "empty") != "empty":
+      print(db["note"])
+      while True:
+        currentline = input()
+        # Check if note is blank
+        if currentline == "/exit":
+          home_page()
         else:
-          newnote = currentline
+          if db.get("note", "empty") != "empty":
+            # If note isn't blank, create new line and write
+            db["note"] = db["note"] + '''
+        ''' + currentline
+          else:
+            # If note is blank, don't create new line, but write
+            db["note"] = currentline
+    else:
+      while True:
+        currentline = input()
+        # Check if note is blank
+        if currentline == "/exit":
+          home_page()
+                  
+        else:
+          if db.get("note", "empty") != "empty":
+            # If note isn't blank, create new line and write
+            db["note"] = db["note"] + '''
+        ''' + currentline
+          else:
+            # If note is blank, don't create new line, but write
+            db["note"] = currentline
+        
 home_page()
