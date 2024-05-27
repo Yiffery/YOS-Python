44e0dee0281a23b896fbceb45269b2e5e4f6707c
Yifei Wang <128399837+Yiffery@users.noreply.github.com>
2024-05-27 14:58:53 +0000

    YOS Python 0.2beta

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # OS SYSTEM IMPORT
 
-
+yosversion = "0.2"
 
 print(" \       /      -----           -----     ")
 print("  \     /     /       \        /          ")
@@ -92,19 +92,8 @@ print("     |          -----           -----     ")
 print("Importing: re")
 print("[========================================] 100%")
 import re
+import platform
 clear()
-  # Imports
-
-
-
-
-
-
-
-
-
-# App Variables
-# Notpad
 
 note = "" 
 # Get terminal size
@@ -117,7 +106,8 @@ def home_page():
   dimensions = re.findall(r'\b\d+\b', str(shutil.get_terminal_size()))
   width = dimensions[0]
   # Print Title and Subtitle
-  print(f'{"YOS Python 0.2beta-3":^{width}}')
+
+  print(f'{"YOS Python {0}":^{width}}'.format(yosversion))
   print("")
 
   # Apps
@@ -125,51 +115,63 @@ def home_page():
   print(f'{"-"*int(width):^{width}}')
 
   print(f'            1. Information{" "*(int(width)-48)}2. Notepad            ')
-
-
   # Input
   select = input("Select an app by inputting the corresponding number: ")
 
   if select == "1":
-    dict = PyDictionary()
-    def dictionary():
-      exec(open("README.md").read())
+    def information():
+      clear()
+      print("Information (Type /exit to exit)")
+      print("="*int(width))
+      print("Python Info:")
+      print("Current Python Version: {0}".format(platform.python_version()))
+      print()
+      print("YOS Info")
+      print("Current YOS Version: {0}".format(yosversion))
+      print()
+      print("Installed Apps:")
+      print("1. Information")
+      print("2. Notepad")
+      exit = input("Press enter to continue")
+      home_page()
+    information()
   elif select == "2":
-
-    clear()
-    print("Notepad (Type /exit to exit)")
-    print("="*int(width))
-    # Check if note is blank
-
-    if db.get("note", "empty") != "empty":
-      print(db["note"])
-      while True:
-        currentline = input()
-        # Check if note is blank
-        if currentline == "/exit":
-          home_page()
-        else:
-          if db.get("note", "empty") != "empty":
-            # If note isn't blank, create new line and write
-            db["note"] = db["note"] + '''
-        ''' + currentline
+    def notepad():
+      
+      clear()
+      print("Notepad (Type /exit to exit)")
+      print("="*int(width))
+      # Check if note is blank
+  
+      if db.get("note", "empty") != "empty":
+        print(db["note"])
+        while True:
+          currentline = input()
+          # Check if note is blank
+          if currentline == "/exit":
+            home_page()
           else:
-            # If note is blank, don't create new line, but write
-            db["note"] = currentline
-    else:
-      while True:
-        currentline = input()
-        # Check if note is blank
-        if currentline == "/exit":
-          home_page()
-
-        else:
-          if db.get("note", "empty") != "empty":
-            # If note isn't blank, create new line and write
-            db["note"] = db["note"] + '''
-        ''' + currentline
+            if db.get("note", "empty") != "empty":
+              # If note isn't blank, create new line and write
+              db["note"] = db["note"] + '''
+          ''' + currentline
+            else:
+              # If note is blank, don't create new line, but write
+              db["note"] = currentline
+      else:
+        while True:
+          currentline = input()
+          # Check if note is blank
+          if currentline == "/exit":
+            home_page()
+  
           else:
-            # If note is blank, don't create new line, but write
-            db["note"] = currentline
-
+            if db.get("note", "empty") != "empty":
+              # If note isn't blank, create new line and write
+              db["note"] = db["note"] + '''
+          ''' + currentline
+            else:
+              # If note is blank, don't create new line, but write
+              db["note"] = currentline
+    notepad()
 home_page()
